ca00926434a90127e1febb7dfd292e8540239d06
Kristijan Todorovic <e11806442@student.tuwien.ac.at>
2023-11-03 21:22:02 +0100

    program is done, issue: infinite loop somewhere

--- mydiff.c
+++ mydiff.c
@@ -1,84 +1,143 @@
 #include <stdio.h>
 #include <getopt.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
-#include <strings.h>
+#include <stdlib.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-static bool writeOutputFile = false;
-static bool caseInsensitive = false;
+static const int num_of_args = 3;
+static const char *program_name;
+static bool case_insensitive = false;
+static bool write_output = false;
 
-static void parseOptions(int argc, char *argv[]);
-static int getShorterStringLength(const char *str1, const char* str2);
-int compare(const char *str1, const char *str2);
-static void printUsageErrorAndFail(char *message, char *programName);
-void printErrorMessageAndFail(char *message, char *programname);
+static void parse_args(int argc, char* argv[], char **input1_path, char **input2_path, 
+        char **output_path);
+static void print_error_and_exit(const char *message);
+static void print_usage_error_and_exit(const char *message);
+static void try_open_file(FILE **file, const char *file_path, char *mode);
+static void work(FILE *input1, FILE *input2, FILE *output);
+static void print_diff(int lin, int chr, FILE *output);
+static int is_eof_or_newline(char c1, char c2);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+    program_name = argv[0];
 
-    parseOptions(argc, argv);
-    printf("reached end\n");
+    char *input1_path = NULL;
+    char *input2_path = NULL;
+    char *output_path = NULL;
+
+    parse_args(argc, argv, &input1_path, &input2_path, &output_path);
+
+    FILE *input1 = NULL;
+    FILE *input2 = NULL;
+    FILE *output = NULL;
+    
+    try_open_file(&input1, input1_path, "r");
+    try_open_file(&input2, input2_path, "r");
+    try_open_file(&output, output_path, "w");
+   
+    work(input1, input2, output);
+
+    fclose(input1);
+    fclose(input2);
+    fclose(output);
 
     return EXIT_SUCCESS;
 }
 
-static void parseOptions(int argc, char *argv[]) {
-    int optionIndex = 0;
+static void parse_args(int argc, char* argv[], char **input1_path, char **input2_path,
+                        char **output_path) {
+    int option_ind = 0;
 
-    while ((optionIndex = getopt(argc, argv, "io:")) != -1) {
-        switch (optionIndex) {
+    while ((option_ind = getopt(argc, argv, "io:")) != -1) {
+        switch (option_ind) {
             case 'i':
-                caseInsensitive = true;
+                case_insensitive = true;
                 break;
             case 'o':
-                writeOutputFile = true;
+                *output_path = optarg;
+                write_output = true;
                 break;
             case '?':
-                printUsageErrorAndFail("Illegal Option", argv[0]);
+                print_usage_error_and_exit("Unknown Argument");
                 break;
             default:
-                printErrorMessageAndFail("Unknown Error", argv[0]);
+                print_error_and_exit("Unknown Error");
                 break;
+        }
+    }
+
+    if (argc - (optind - 1) < num_of_args) {
+        print_usage_error_and_exit("Insufficient Arguments");
+    }
+
+    *input1_path = argv[optind];
+    *input2_path = argv[optind + 1];
+}
+
+static void print_error_and_exit(const char *message) {
+    fprintf(stderr, "%s: %s\n", program_name, message);
+    exit(EXIT_FAILURE);
+}
+
+static void print_usage_error_and_exit(const char *message) {
+    fprintf(stderr, "%s: %s - USAGE: %s [-i] [-o output.txt] input1.txt input2.txt\n",
+            program_name, message, program_name);
+    exit(EXIT_FAILURE);
+}
+
+static void try_open_file(FILE **file, const char *file_path, char *mode) {
+    if (file_path != NULL) {
+        *file = fopen(file_path, mode);
+    
+        if (*file == NULL) {
+            print_error_and_exit("Error occurred trying to open file");
         }
     }
 }
 
-static int getShorterStringLength(const char *str1, const char *str2) {
-    int strLength1 = strlen(str1);
-    int strLength2 = strlen(str2);
-
-
-    if (strLength1 < strLength2) {
-        return strLength1;
-    } else if (strLength2 < strLength1) {
-        return strLength2;
-    }
+static void work(FILE *input1, FILE *input2, FILE *output) {
+    int line = 1;
+    int character = 1;
     
-    return strLength1;
+    char c1 = fgetc(input1);
+    char c2 = fgetc(input2);
+    
+    while (c1 != EOF || c2 != EOF) {
+        character = 1;
+        while (is_eof_or_newline(c1, c2) == 0) {
+            if (case_insensitive) {
+                if (tolower(c1) != tolower(c2)) {
+                    print_diff(line, character, output);
+                    break;
+                }
+            } else {
+                if (c1 != c2) {
+                    print_diff(line, character, output);
+                    break;
+                }
+            }
+            c1 = fgetc(input1);
+            c2 = fgetc(input2);
+            character++;
+        }
+        line++;
+    }
 }
 
-int compare(const char *str1, const char *str2) {
-
-    int result;
-    int n = getShorterStringLength(str1, str2);
-
-    if (caseInsensitive) {
-        result = strncasecmp(str1, str2, n);
+static void print_diff(int lin, int chr, FILE *output) {
+    if (write_output) {
+        fprintf(output, "Line: %d, characters: %d", lin, chr);
     } else {
-        result = strncmp(str1, str2, n);
+        printf("Line: %d, characters: %d\n", lin, chr);
     }
-    
-    return result;
 }
 
-static void printUsageErrorAndFail(char *message, char *programName) {
-    fprintf(stderr, "%s: USAGE: %s [-i] [-o outfile] input1 input2\n" ,message, programName);
-    exit(EXIT_FAILURE);
+static int is_eof_or_newline(char c1, char c2) {
+    if (c1 == '\n') return 1;
+    if (c1 == EOF) return 1;
+    if (c2 == '\n') return 1;
+    if (c2 == EOF) return 1;
+    return 0;
 }
-
-void printErrorMessageAndFail(char *message, char *programName) {
-    fprintf(stderr, "%s: %s\n", programName, message);
-    exit(EXIT_FAILURE);
-}
-
